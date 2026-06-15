bf001221ceb447a009393b11a51d1e396e039197
qinyongliang <1130140133@qq.com>
2026-06-15 11:59:32 +0800

    Proxy agent downloads from releases

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,11 +1,17 @@
 package server
 
+const DefaultVersion = "dev"
+
 type Config struct {
-	HTTPListen string
-	SSHListen  string
-	PublicHost string
-	AgentToken string
-	AgentPath  string
+	HTTPListen      string
+	SSHListen       string
+	PublicHost      string
+	AgentToken      string
+	AgentPath       string
+	AgentCachePath  string
+	Version         string
+	ReleaseBaseURL  string
+	ReleaseProxyURL string
 }
 
 func (c Config) publicHost() string {
@@ -13,4 +19,25 @@ func (c Config) publicHost() string {
 		return c.PublicHost
 	}
 	return "localhost"
+}
+
+func (c Config) version() string {
+	if c.Version != "" {
+		return c.Version
+	}
+	return DefaultVersion
+}
+
+func (c Config) releaseBaseURL() string {
+	if c.ReleaseBaseURL != "" {
+		return c.ReleaseBaseURL
+	}
+	return "https://github.com/qinyongliang/gosshd/releases/download"
+}
+
+func (c Config) releaseProxyURL() string {
+	if c.ReleaseProxyURL != "" {
+		return c.ReleaseProxyURL
+	}
+	return "https://gh-proxy.com/"
 }
